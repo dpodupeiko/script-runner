8166f885542a1720438d6a44eceb4d9cb65709a0
dpodupeiko <d.podupeiko@gmail.com>
2026-01-07 12:45:24 -0800

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,1 +1,8 @@
+const mainHeading = document.getElementById('main-heading');
+const changeButton = document.getElementById('change-button');
 
+// Add an event listener to the button for the 'click' event
+changeButton.addEventListener('click', function() {
+    // Change the text content of the H1 element
+    mainHeading.textContent = 'The text has been changed!';
+});
